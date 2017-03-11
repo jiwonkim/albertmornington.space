d34639049bee63a77e7d0cf06357782d50edec77
Jiwon Kim <kittyvision@gmail.com>
2017-03-11 13:39:22 -0800

    Merges adjacent posts (timewise) for timeline display

--- db/post.py
+++ db/post.py
@@ -1,7 +1,9 @@
 from flask import url_for
 
+SECONDS_IN_WEEK = 604800
+
 def get_timeline_data(r):
-    timeline_data = []
+    post_dicts = []
     post_keys = r.keys('post:*')
     for post_key in post_keys:
         timestamp, title, alias, site = r.hmget(
@@ -11,11 +13,34 @@ def get_timeline_data(r):
             'alias',
             'site',
         )
-        timeline_data.append({
-            'timestamp': timestamp,
+        post_dicts.append({
+            'timestamp': int(timestamp),
             'title': title,
             'url': url_for('show_post', post_alias=alias),
             'site': site,
         })
 
-    return timeline_data
+    return _merge_adjacent(post_dicts)
+
+def _merge_adjacent(post_dicts):
+    merged = []
+    sorted_post_dicts = sorted(
+        post_dicts,
+        key=lambda d: d['timestamp'],
+    )
+
+    for curr in sorted_post_dicts:
+        prev = merged and merged[-1]
+        if prev and curr['timestamp'] - prev['timestamp'] < SECONDS_IN_WEEK:
+            prev['posts'].append(curr)
+        else:
+            merged.append({
+                'timestamp': curr['timestamp'],
+                'posts': [{
+                    'title': curr['title'],
+                    'url': curr['url'],
+                    'site': curr['site']
+                }]
+            })
+
+    return merged
--- static/timeline.js
+++ static/timeline.js
@@ -1,26 +1,14 @@
 $(document).ready(() => {
+    const HEIGHT = 500;
+
     const MARGIN_LEFT = 100; // margin around the axis
     const MARGIN_TOP = 10, MARGIN_BOTTOM = 10;
-    const HEIGHT = 500;
+
+    const POST_TICK_LENGTH = 30;
 
     const SECONDS_IN_MONTH = 2592000;
 
-    const timeline = d3.select('.navigation--timeline-view')
-        .append('svg:svg')
-            .attr('width', '100%')
-            .attr('height', HEIGHT);
-
-    const minT = d3.min(timelineData, (d) => d.timestamp);
-    const timelineScale = d3.scaleTime()
-        .domain([
-            new Date(),
-            new Date((minT - SECONDS_IN_MONTH) * 1000)
-        ])
-        .range([MARGIN_TOP, HEIGHT - MARGIN_BOTTOM]);
-
-    let tickValues = timelineData.map((d) => d.timestamp);
-
-    let timelineTickFormat = (d) => {
+    let timelineTickFormat = function(d) {
         const month = d.getMonth();
         if (month === 0) {
             return d3.timeFormat('%Y')(d);
@@ -31,12 +19,43 @@ $(document).ready(() => {
         return d3.timeFormat('%b')(d);
     }
 
+    const timeline = d3.select('.navigation--timeline-view')
+        .append('svg:svg')
+            .attr('width', '100%')
+            .attr('height', HEIGHT);
+
+    const minT = d3.min(timelineData, d => d.timestamp);
+    const timelineScale = d3.scaleTime()
+        .domain([
+            new Date(),
+            new Date((minT - SECONDS_IN_MONTH) * 1000)
+        ])
+        .range([MARGIN_TOP, HEIGHT - MARGIN_BOTTOM]);
+
     const axis = d3.axisLeft(timelineScale)
         .ticks(d3.timeMonth.every(1))
         .tickFormat(timelineTickFormat);
 
     timeline.append('g')
         .attr('class', 'axis')
-        .attr('transform', 'translate(' + MARGIN_LEFT + ', 0)')
+        .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
         .call(axis);
+
+    const posts = timeline.selectAll('.post')
+      .data(timelineData)
+      .enter().append('g')
+        .attr('class', 'post')
+        .attr('transform', function(d) {
+            const date = new Date(d.timestamp * 1000);
+            return 'translate(0, ' + timelineScale(date) + ')';
+        })
+        .each(function(d, i) {
+            d3.select(this).append('line')
+                .attr('x0', 0)
+                .attr('x1', POST_TICK_LENGTH)
+                .attr('y0', 0)
+                .attr('y1', 0)
+                .attr('class', 'post-tick')
+                .attr('transform', `translate(${MARGIN_LEFT - POST_TICK_LENGTH}, 0)`);
+        });
 });
