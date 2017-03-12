50f40dfdb4de11bd1f372a4af537881432b86513
Jiwon Kim <kittyvision@gmail.com>
2017-03-11 19:32:24 -0800

    Links working in timeline nav

--- static/timeline.js
+++ static/timeline.js
@@ -1,14 +1,18 @@
 $(document).ready(() => {
+    const WIDTH = 448;
     const HEIGHT = 500;
 
-    const MARGIN_LEFT = 100; // margin around the axis
+    // margin around the timeline
+    const MARGIN_LEFT = 50;
+    const MARGIN_RIGHT = 20;
     const MARGIN_TOP = 10, MARGIN_BOTTOM = 10;
 
     const POST_TICK_LENGTH = 30;
+    const POST_HEIGHT = 25;
 
     const SECONDS_IN_MONTH = 2592000;
 
-    let timelineTickFormat = function(d) {
+    const timelineTickFormat = function(d) {
         const month = d.getMonth();
         if (month === 0) {
             return d3.timeFormat('%Y')(d);
@@ -17,6 +21,42 @@ $(document).ready(() => {
             return '';
         }
         return d3.timeFormat('%b')(d);
+    }
+
+    const renderPostsForDataPoint = function(selection, d) {
+        selection.append('line')
+            .attr('x0', 0)
+            .attr('x1', POST_TICK_LENGTH)
+            .attr('y0', 0)
+            .attr('y1', 0)
+            .attr('class', 'post-tick')
+            .attr('transform', `translate(${MARGIN_LEFT - POST_TICK_LENGTH}, 0)`);
+
+        const midpoint = (d.posts.length - 1) / 2;
+        d.posts.forEach((post, index) => {
+            const yOffset = (index - midpoint) * POST_HEIGHT;
+            const line = d3.line()
+                .x(d => d.x)
+                .y(d => d.y);
+
+            selection.append('path')
+                .datum([
+                    {x: MARGIN_LEFT + 1, y: 0},
+                    {x: MARGIN_LEFT + POST_TICK_LENGTH, y: yOffset},
+                    {x: WIDTH - MARGIN_RIGHT, y: yOffset},
+                ])
+                .attr('class', 'post-underline')
+                .attr('d', line);
+
+            selection.append('text')
+                .attr('x', MARGIN_LEFT + POST_TICK_LENGTH)
+                .attr('y', yOffset - 5)
+                .attr('class', 'post-title')
+                .text(post.title.toLowerCase())
+                .on('click', () => {
+                    window.location = post.url;
+                });
+        });
     }
 
     const timeline = d3.select('.navigation--timeline-view')
@@ -50,12 +90,6 @@ $(document).ready(() => {
             return 'translate(0, ' + timelineScale(date) + ')';
         })
         .each(function(d, i) {
-            d3.select(this).append('line')
-                .attr('x0', 0)
-                .attr('x1', POST_TICK_LENGTH)
-                .attr('y0', 0)
-                .attr('y1', 0)
-                .attr('class', 'post-tick')
-                .attr('transform', `translate(${MARGIN_LEFT - POST_TICK_LENGTH}, 0)`);
+            renderPostsForDataPoint(d3.select(this), d);
         });
 });
